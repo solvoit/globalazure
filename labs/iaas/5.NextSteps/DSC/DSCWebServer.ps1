6aa8540a991f68239b5c92600eee0e4181c40170
Jesper Jensen <jj@solvoit.com>
2016-04-16 01:18:06 +0200

    labs

---
+++ labs/iaas/5.NextSteps/DSC/DSCWebServer.ps1
@@ -0,0 +1,20 @@
+﻿Configuration DSCWebServer
+{
+    param(
+        $machineName
+    )
+    Node $machineName
+    {
+        WindowsFeature IIS
+        {
+            Ensure = "Present"
+            Name = "Web-Server"
+        }
+
+        WindowsFeature IISMgmtTools
+        {
+            Ensure = "Present"
+            Name = "Web-Mgmt-Tools"
+        }
+    }
+}
